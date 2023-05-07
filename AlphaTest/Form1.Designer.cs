c03e36310f6c42f6f130f8f6bb36f08b662842f2
Водопьян Артём <vodopianartem@gmail.com>
2023-05-07 17:55:35 +0300

    Добавьте файлы проекта.

---
+++ AlphaTest/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace AlphaTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(40, 380);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Загрузка";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 350);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Enabled = false;
+            this.btnFilter.Location = new System.Drawing.Point(137, 380);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnFilter.TabIndex = 2;
+            this.btnFilter.Text = "Фильтр";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Enabled = false;
+            this.btnSort.Location = new System.Drawing.Point(237, 380);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 3;
+            this.btnSort.Text = "Сортировка";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Enabled = false;
+            this.btnExport.Location = new System.Drawing.Point(349, 380);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Экспорт";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ AlphaTest/Form1.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+
+
+
+namespace AlphaTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataSet ds = new DataSet("Channel");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = ds.Tables[0];
+            dataGridView1.Columns[0].Width = 150;
+            dataGridView1.Columns[1].Width = 150;
+            dataGridView1.Columns[3].Width = 150;
+            dataGridView1.Columns[4].Width = 150; 
+
+            btnExport.Enabled = true;
+            btnFilter.Enabled = true;
+      
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            btnSort.Enabled = true;
+            string text;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                for (int i=0; i< dataGridView1.Rows.Count; i++)
+                {
+                    text = dataGridView1[3, i].Value.ToString();
+
+                    if (!text.Contains("Политика"))
+                    {
+                        dataGridView1.Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+                    }
+                }
+            }
+            dataGridView1.DataSource = ds.Tables[0];   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ds.ReadXml(System.Windows.Forms.Application.StartupPath + @"\data.xml");
+        }
+
+        bool flagSort = false;
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-CA");
+            dataGridView1.Columns[4].DefaultCellStyle.Format = "ddd, dd MMM yyyy HH:mm:ss";
+
+            System.Data.DataTable dt = ds.Tables[0];
+            System.Data.DataTable dtCloned = dt.Clone();
+            dtCloned.Columns[4].DataType = typeof(DateTime);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                dtCloned.ImportRow(row);
+            }
+            dataGridView1.DataSource = dtCloned;
+
+            if (!flagSort)
+            {
+                this.dataGridView1.Sort(this.dataGridView1.Columns["pubDate"], ListSortDirection.Ascending);
+                flagSort = true;
+            }
+            else
+            {
+                this.dataGridView1.Sort(this.dataGridView1.Columns["pubDate"], ListSortDirection.Descending);
+                flagSort = false;
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            int columns = dataGridView1.Columns.Count;
+            int rows = dataGridView1.Rows.Count;
+            string[,] array = new string[rows, columns];
+
+            for (int i = 0; i <= dataGridView1.Rows.Count-1; i++)
+                for (int j = 0; j <= dataGridView1.Columns.Count-1; j++)
+                {
+                    array[i, j] = dataGridView1[j, i].Value.ToString();
+                }
+
+            Word.Application application = new Word.Application();
+            Object missing = Type.Missing;
+            application.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+            Word.Document document = application.ActiveDocument;
+            Word.Range range = application.Selection.Range;
+            Object behiavor = Word.WdDefaultTableBehavior.wdWord9TableBehavior;
+            Object autoFitBehiavor = Word.WdAutoFitBehavior.wdAutoFitFixed;
+            document.Tables.Add(range, rows, columns, ref behiavor, ref autoFitBehiavor);
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    document.Tables[1].Cell(i + 1, j + 1).Range.Text = array[i, j].ToString();
+            application.Visible = true;
+        }
+    }
+}
+
